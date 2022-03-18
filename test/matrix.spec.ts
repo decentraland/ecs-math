2c8a4d4bdc1b990366f56cb8de1a0b1138c5ade8
Leandro Mendoza <leandro@decentraland.org>
2022-03-18 10:20:12 -0300

    new matrix representation in tests

--- test/matrix.spec.ts
+++ test/matrix.spec.ts
@@ -2,20 +2,19 @@ import { Matrix } from '../src/Matrix'
 
 const results = {
   identity:
-    '(1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0)'
+    '([1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0])'
 }
 
 const normalize = (v: string) => (v === '-0.0' ? '0.0' : v)
+const v = (matrix: Matrix.ReadonlyMatrix, index: number) =>
+  normalize(matrix._m[index].toFixed(1).substr(0, 6))
 
-function matrixToString(mat: Matrix.ReadonlyMatrix) {
-  let res = '('
-  for (const [i, value] of mat._m.entries()) {
-    if (i < 15) {
-      res += `${normalize(value.toFixed(1).substr(0, 6))}, `
-    } else {
-      res += `${normalize(value.toFixed(1).substr(0, 6))})`
-    }
-  }
+function matrixToString(m: Matrix.ReadonlyMatrix) {
+  let res = ''
+  res += `([${v(m, 0)}, ${v(m, 1)}, ${v(m, 2)}, ${v(m, 3)}], `
+  res += `[${v(m, 4)}, ${v(m, 5)}, ${v(m, 6)}, ${v(m, 7)}], `
+  res += `[${v(m, 8)}, ${v(m, 9)}, ${v(m, 10)}, ${v(m, 11)}], `
+  res += `[${v(m, 12)}, ${v(m, 13)}, ${v(m, 14)}, ${v(m, 15)}])`
   return res
 }
 
