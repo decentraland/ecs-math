4310e759f8976f9a6f3d068acb62b5312ba8024a
Leandro Mendoza <leandro@decentraland.org>
2022-03-24 01:04:28 -0300

    complete vector3 implementation and add tests

--- test/vector3.spec.ts
+++ test/vector3.spec.ts
@@ -43,7 +43,7 @@ describe('ECS Vector3 - Next tests', () => {
   })
 
   it('Vector3.opposite(ones) = -ones', () => {
-    expect(vector3ToString(Vector3.opposite(Vector3.One()))).toEqual(
+    expect(vector3ToString(Vector3.negate(Vector3.One()))).toEqual(
       results.minusOnes
     )
   })
@@ -91,5 +91,66 @@ describe('ECS Vector3 - Next tests', () => {
         )
       )
     ).toEqual(results.rotatedWithQuat04)
+  })
+
+  it('Vector3 operations isNonUniform', () => {
+    expect(Vector3.isNonUniform(Vector3.create(1, -2, -5))).toEqual(true)
+    expect(Vector3.isNonUniform(Vector3.create(1, 1, -5))).toEqual(true)
+    expect(Vector3.isNonUniform(Vector3.create(1, -1, -1))).toEqual(false)
+    expect(Vector3.isNonUniform(Vector3.create(1, 1, 1))).toEqual(false)
+  })
+
+  it('Vector3 operations subtract', () => {
+    const vector1 = Vector3.create(10, 10, -10) as Vector3.ReadonlyVector3
+    const vector2 = Vector3.create(2, 2, 2) as Vector3.ReadonlyVector3
+    const result = Vector3.create()
+
+    Vector3.subtractToRef(vector1, vector2, result)
+    expect(result.x).toEqual(8)
+    expect(result.y).toEqual(8)
+    expect(result.z).toEqual(-12)
+
+    Vector3.subtractFromFloatsToRef(vector1, -10, -10, -10, result)
+    expect(result.x).toEqual(20)
+    expect(result.y).toEqual(20)
+    expect(result.z).toEqual(0)
+  })
+
+  it('Vector3 operations addToRef', () => {
+    const vector1 = Vector3.create(10, 10, -10) as Vector3.ReadonlyVector3
+    const vector2 = Vector3.create(2, 2, 2) as Vector3.ReadonlyVector3
+    const result = Vector3.create()
+
+    Vector3.addToRef(vector1, vector2, result)
+    expect(result.x).toEqual(12)
+    expect(result.y).toEqual(12)
+    expect(result.z).toEqual(-8)
+  })
+
+  it('Vector3 operations addToRef', () => {
+    const result = Vector3.create()
+    Vector3.copyFromFloats(20, 21, 23.5, result)
+    expect(result.x).toEqual(20)
+    expect(result.y).toEqual(21)
+    expect(result.z).toEqual(23.5)
+  })
+
+  it('Vector3 getClipFactor', () => {
+    const vector1 = Vector3.create(10, 10, -10) as Vector3.ReadonlyVector3
+    const vector2 = Vector3.create(2, 2, 2) as Vector3.ReadonlyVector3
+    const axis = Vector3.One()
+    expect(Vector3.getClipFactor(vector1, vector2, axis, 1)).toEqual(2.25)
+  })
+
+  it('Vector3 getAngleBetweenVectors', () => {
+    const vector1 = Vector3.create(5, 5, 5) as Vector3.ReadonlyVector3
+    const vector2 = Vector3.Zero() as Vector3.ReadonlyVector3
+    const normal = Vector3.cross(vector1, vector2)
+    expect(
+      Vector3.getAngleBetweenVectors(vector1, vector2, normal)
+    ).toBeCloseTo(-Math.PI / 2)
+    expect(
+      Vector3.getAngleBetweenVectors(vector1, vector2, Vector3.negate(normal))
+    ).toBeCloseTo(-Math.PI / 2)
   })
 })
